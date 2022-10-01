3aa08d8daf34f4fa22c8124898a3ad0f0b8c7e01
khowling <khowling@gmail.com>
2022-10-01 13:51:28 +0100

    add prisma

--- server/trcpRouter.ts
+++ server/trcpRouter.ts
@@ -10,6 +10,10 @@ import * as http from 'http'
 
 import { EventEmitter } from 'events';
 
+// ORM
+import { Prisma, PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 // create a global event emitter (could be replaced by redis, etc)
 const ee = new EventEmitter();
 
@@ -19,46 +23,92 @@ interface Post {
   data: String;
 }
 
-interface User {
-    id: string;
-    name: string;
-}
-   
-const userList: User[] = [
-  {
-    id: '1',
-    name: 'KATT',
-  },
-];
-
-
-
 
 const t = trpc.initTRPC.create();
 
+const defaultUserSelect = Prisma.validator<Prisma.UserSelect>()({
+  id: true,
+  name: true,
+  email: true
+});
+
 const appRouter = t.router({
+  list: t.procedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+      }),
+    )
+    .query(async ({ input }) => {
+      /**
+       * For pagination docs you can have a look here
+       * @see https://trpc.io/docs/useInfiniteQuery
+       * @see https://www.prisma.io/docs/concepts/components/prisma-client/pagination
+       */
+
+      const limit = input.limit ?? 50;
+      const { cursor } = input;
+
+      const users = await prisma.user.findMany({
+        select: defaultUserSelect,
+        // get an extra item at the end which we'll use as next cursor
+        take: limit + 1,
+        where: {},
+        cursor: cursor
+          ? {
+              id: cursor,
+            }
+          : undefined
+      });
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (users.length > limit) {
+        // Remove the last item and use it as next cursor
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const nextItem = users.pop()!;
+        nextCursor = nextItem.id;
+      }
+
+      return {
+        items: users.reverse(),
+        nextCursor,
+      };
+    }),
 
     userById: t.procedure
-      .input((val: unknown) => {
-        if (typeof val === 'string') return val;
-        throw new Error(`Invalid input: ${typeof val}`);
-      })
-      .query((req) => {
-        const { input } = req;
-        const user = userList.find((u) => u.id === input);
-   
+      .input(z.object({
+          id: z.string()
+        })
+      )
+      .query(async ({input}) => {
+        const { id } = input;
+        const user =  await prisma.user.findUnique({
+          select: defaultUserSelect,
+          where: {id}
+        });
+        if (!user) {
+          throw new trpc.TRPCError({
+            code: 'NOT_FOUND',
+            message: `No users with id '${id}'`,
+          });
+        }
         return user;
       }),
 
     userCreate: t.procedure
-      .input(z.object({ name: z.string() }))
-      .mutation((req) => {
+      .input(
+        z.object({
+          email: z.string().min(1).max(32),
+          name: z.string().min(1)
+        })
+      )
+      .mutation(async ({input}) => {
         const id = `${Math.random()}`;
-        const user: User = {
-          id,
-          name: req.input.name,
-        };
-        userList.push(user);
+
+        const user = await prisma.user.create({
+          data: input
+        });
         return user;
       }),
 
